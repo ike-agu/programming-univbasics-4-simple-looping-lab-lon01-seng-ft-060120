d17f866b1436ed38b95fb24277e6ffea64912182
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-12 21:29:14 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -27,3 +27,12 @@ def output_array(array)
   end
 end
 
+
+def return_string_array(array)
+  count = 0
+  while count < array.length do
+    array[count] = array[count].to_s
+    count += 1
+  end
+  array
+end
